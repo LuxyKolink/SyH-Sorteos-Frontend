86c6d6cdb63163a74bae97b04937abc600c9d8f2
Santiago Molano García <santiagolikeaboss22@gmail.com>
2024-01-10 17:06:00 -0500

    sync

--- src/app/features/table/table.component.ts
+++ src/app/features/table/table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Raffle } from '../../data/raffle/interface/raffle';
 
 @Component({
   selector: 'app-table',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './table.component.css'
 })
 export class TableComponent {
-
+  @Input() raffle!:Raffle;
 }
